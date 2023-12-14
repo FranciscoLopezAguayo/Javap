1484c7ae2468a6efac3a4061a88a531ad05b9a16
francisco <lopez.aguayo.francisco@gmail.com>
2023-12-14 01:10:56 -0300

    previa actualizacion librerias

---
+++ fjla-BACKUP/src/main/java/com/javap/fjla/FjlaApplication.java
@@ -0,0 +1,13 @@
+package com.javap.fjla;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FjlaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FjlaApplication.class, args);
+	}
+
+}
---
+++ fjla-BACKUP/src/main/java/com/javap/fjla/persistance/entity/User.java
@@ -0,0 +1,117 @@
+package com.javap.fjla.persistance.entity;
+
+import com.javap.fjla.persistance.util.Role;
+import jakarta.persistence.*;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.stream.Collectors;
+
+@Entity
+@Table(name = "_user_")
+public class User implements UserDetails {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true)
+    private String username;
+    private String name;
+    private String password;
+    private String Email;
+
+    @Enumerated(EnumType.STRING)
+    private Role role;
+
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+
+        if (role == null ) return null;
+
+        if (role.getPermissions() == null ) return null;
+
+        return role.getPermissions().stream()
+                .map(each ->each.name())
+                .map(each -> new SimpleGrantedAuthority(each))
+                //.map(each ->{
+               //     String permission = each.name();
+               //     return new SimpleGrantedAuthority(permission);
+
+               // })
+                .collect(Collectors.toList());
+    }
+
+    @Override
+    public String getPassword() {
+        return password;
+    }
+
+    @Override
+    public String getUsername() {
+        return username;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getEmail() {
+        return Email;
+    }
+
+    public void setEmail(String email) {
+        Email = email;
+    }
+
+    public Role getRole() {
+        return role;
+    }
+
+    public void setRole(Role role) {
+        this.role = role;
+    }
+}
